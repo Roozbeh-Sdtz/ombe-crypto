f673967661588d3b684133aba259cfad61cd8c06
roozbeh <roozbeh1377@gmail.com>
2020-07-31 02:44:13 +0430

    clean up

--- src/features/Utill/Stepper.js
+++ src/features/Utill/Stepper.js
@@ -5,7 +5,6 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import StepContent from '@material-ui/core/StepContent';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
@@ -98,14 +97,6 @@ export default function VerticalLinearStepper() {
                     </Step>
                 ))}
             </Stepper>
-            {/*{activeStep === steps.length && (*/}
-            {/*    <Paper square elevation={0} className={classes.resetContainer}>*/}
-            {/*        <Typography>All steps completed - you&apos;re finished</Typography>*/}
-            {/*        <Button onClick={handleReset} className={classes.button}>*/}
-            {/*            Reset*/}
-            {/*        </Button>*/}
-            {/*    </Paper>*/}
-            {/*)}*/}
         </div>
     );
 }
